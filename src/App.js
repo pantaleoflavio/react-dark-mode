111d5d88e85bb3987af4954e23efde2260c00f40
flavio <flavio.pantaleo@yahoo.com>
2023-09-22 20:17:53 +0200

    add change theme functionality

--- src/App.js
+++ src/App.js
@@ -6,10 +6,25 @@ import Articolo from "./Articolo";
 // returna il suo valore o di default return 'light-mode'
 
 function App() {
+  const [theme, setTheme] = useState('light-mode')
+
+  // func that change theme according to the value
+  const changeTheme = () => {
+    if (theme === 'light-mode') {
+      setTheme('dark-mode')
+    } else {
+      setTheme('light-mode')
+    }
+  };
+
+  // changing theme state add a class to html tag
+  useEffect(() => {
+    document.documentElement.className = theme;
+  },[theme])
   return (
     <section className="section-center">
       <div className="container">
-      <button className="btn">
+      <button onClick={changeTheme} className="btn">
           Change
         </button>
         <section className="article-section">
